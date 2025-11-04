d04a5edad79efc86f14cd52b8ed34923791362ae
Cedric <crubbertire@rogers.com>
2025-11-04 17:31:40 -0500

    set up test connection to db

--- test/app.test.js
+++ test/app.test.js
@@ -9,6 +9,18 @@ const user = require("../models/user");
 
 before(() => {
   // Connect to the DB
+  mongoose.connect(process.env.MONGODB_URI);
+
+  // Once connected to the DB
+  mongoose.connection.once("open", async () => {
+    try {
+      // Create a new user
+      await UserActivation.create();
+      done();
+    } catch (error) {
+      done(error);
+    }
+  })
 })
 
 // Test Cases go here
