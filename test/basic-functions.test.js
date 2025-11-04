1b813a02f497142a660e3fdb3865bb355b70ff41
Cedric <crubbertire@rogers.com>
2025-11-04 17:18:01 -0500

    Implemented Multi-test cases

--- test/basic-functions.test.js
+++ test/basic-functions.test.js
@@ -1,8 +1,41 @@
 const { expect } = require("chai");
-const {validateEmail } = require("../src/basic-functions.js");
+const { validateEmail, validatePassword, validatePhoneNumber } = require("../src/basic-functions.js");
 
-it("should return true for a valid email", () => {
-  const validEmail = "test@email.com";
-  const result = validateEmail(validEmail);
-  expect(result).to.be.true; // CHAI Assertion
+describe("Tests For: validateEmail function", () => {
+  it("should return true for a valid email", () => {
+    const validEmail = "test@example.com";
+    const result = validateEmail(validEmail);
+    expect(result).to.be.true;
+  });
+});
+
+describe("Tests For: validatePassword function", () => {
+  it("should return true for a valid password", () => {
+    const validPassword = "password123";
+    const result = validatePassword(validPassword);
+    expect(result).to.be.true;
+  });
+});
+
+describe("Tests For: validatePhoneNumber function:", () => {
+  // Test case 1
+  it("should return true for a valid phone number", () => {
+    const validPhoneNumber = "1234567890";
+    const result = validatePhoneNumber(validPhoneNumber);
+    expect(result).to.be.true;
+  });
+
+  // Test case 2
+  it("should return false for a phone number with a length other than 10 digits", () => {
+    const invalidPhoneNumber = "123456789";
+    const result = validatePhoneNumber(invalidPhoneNumber);
+    expect(result).to.be.false;
+  });
+
+  // Test case 3
+  it("should return false for a phone number with non-digit characters", () => {
+    const invalidPhoneNumber = "123abc4567";
+    const result = validatePhoneNumber(invalidPhoneNumber);
+    expect(result).to.be.false;
+  });
 })
